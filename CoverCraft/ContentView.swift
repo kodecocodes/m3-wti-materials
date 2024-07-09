bcb5c1d877959211e8af0f21b0c66a01867b0d0c
SuzGupta <suzskyer@gmail.com>
2024-07-09 10:15:47 -0700

    replacing inadvertently deleted files

--- CoverCraft/ContentView.swift
+++ CoverCraft/ContentView.swift
@@ -1,27 +1,100 @@
-//
-//  ContentView.swift
-//  CoverCraft
-//
-//  Created by Susannah Skyer Gupta on 7/5/24.
-//
+/// Copyright (c) 2024 Kodeco Inc.
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
 
 import SwiftUI
 
 struct ContentView: View {
   var body: some View {
     NavigationView {
-      VStack {
-        NavigationLink(destination: CoverLetterView()) {
-          Text("Generate Cover Letter")
+      ZStack() {
+        LinearGradient(
+          gradient: Gradient(colors: [Color(red: 1.0, green: 0.5, blue: 0.0).opacity(0.3), Color(red: 1.0, green: 0.5, blue: 0.0).opacity(0.1)]),
+          startPoint: .top,
+          endPoint: .bottom
+        )
+        .edgesIgnoringSafeArea(.all)
+        VStack(spacing: 20) {
+          NavigationLink(destination: CoverLetterView()) {
+            HStack {
+              Image(systemName: "pencil")
+              Text("Craft Cover Letter")
+                .fontWeight(.semibold)
+            }
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color.white)
+            .foregroundColor(.blue)
+            .cornerRadius(8)
+            .overlay(
+              RoundedRectangle(cornerRadius: 8)
+                .stroke(Color.blue, lineWidth: 1)
+            )
+          }
+          NavigationLink(destination: SkillsView()) {
+            HStack {
+              Image(systemName: "lightbulb")
+              Text("Define Your Skills")
+                .fontWeight(.semibold)
+            }
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color.white)
+            .foregroundColor(.blue)
+            .cornerRadius(8)
+            .overlay(
+              RoundedRectangle(cornerRadius: 8)
+                .stroke(Color.blue, lineWidth: 1)
+            )
+          }
+          NavigationLink(destination: JobPostingView()) {
+            HStack {
+              Image(systemName: "magnifyingglass")
+              Text("Rsearch an Opportunity")
+                .fontWeight(.semibold)
+            }
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color.white)
+            .foregroundColor(.blue)
+            .cornerRadius(8)
+            .overlay(
+              RoundedRectangle(cornerRadius: 8)
+                .stroke(Color.blue, lineWidth: 1)
+            )
+          }
         }
-        NavigationLink(destination: SkillsView()) {
-          Text("Define Your Skills")
-        }
-        NavigationLink(destination: JobPostingView()) {
-          Text("Learn More About an Opportunity")
-        }
+        .padding()
       }
-      .navigationTitle("Writing Tools Sample App")
+      .navigationTitle("CoverCraft")
     }
   }
 }
